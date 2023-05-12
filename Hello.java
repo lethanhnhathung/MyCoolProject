6e21b15c664d43e145fb1c688ef53638c1810016
lethanhnhathung <hungltngcd210409@fpt.edu.vn>
2023-05-12 13:15:41 +0700

    Your message about the commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package WEEK1;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String ht;
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Moi ban nhap ten:");
+		ht = sc.nextLine();
+		System.out.println("Hello "+ ht);
+
+	}
+
+}
